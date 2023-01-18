1e387c0c5c53d49dd607f5d8dae179f2b9daf320
Émilia Beguin <emilia.beguin@gmail.com>
2023-01-18 11:52:29 +0100

    WIP

---
+++ info/script.js
@@ -0,0 +1,3 @@
+gsap.fromTo(".I", { opacity: 0 }, { opacity: 1 }).delay(1);
+gsap.fromTo("nav", { opacity: 0 }, { opacity: 1 }).delay(1.5);
+gsap.fromTo(".scroll-downs", { opacity: 0 }, { opacity: 1 }).delay(1.9);
